fa2941a14e4cbf387b92036923c3f4109e777b40
Huy M. Nguyen <nmhuy14@gmail.com>
2016-07-19 11:01:37 +0900

    finish enshu5 1,2; set textfield to null when clicked

--- src/enshu5/SimpleEditor.java
+++ src/enshu5/SimpleEditor.java
@@ -3,6 +3,8 @@ package enshu5;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -10,20 +12,66 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
 
-public class SimpleEditor {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
+public class SimpleEditor {
+	private JTextArea ta = null;
+	private JTextField fileName = null;
+	private String fname;
     public SimpleEditor() {
    	 JFrame jf = new JFrame("Simple Editor");
    	 
    	 JPanel fileNameBar = new JPanel();
-   	 JTextArea ta = new JTextArea("Enter your text here");
+   	 ta = new JTextArea("Enter your text here");
    	 
    	 jf.setLayout(new BorderLayout());
    	 jf.add(fileNameBar, BorderLayout.NORTH);
    	 jf.add(new JScrollPane(ta), BorderLayout.CENTER);
    	 
    	 JButton load = new JButton("Load");
-   	 JTextField fileName = new JTextField("Enter your text here");
+   	 fileName = new JTextField("Enter your file name here");
+   	 // set fileName text field empty when clicked
+   	 fileName.addMouseListener(new MouseAdapter() {
+   		public void mouseClicked(MouseEvent e) {
+   			fileName.setText(null);
+   		}
+   	 });
+   	 load.addActionListener(new ActionListener() {
+   		 public void actionPerformed(ActionEvent e) {
+   			 ta.setText(null);
+   			 //read file
+   			 fname = fileName.getText();
+   			 FileReader fr;
+   			 try {
+   				 fr = new FileReader(fname);
+   			 } catch(FileNotFoundException f) {
+   				System.out.println("Can not open " + fname);
+   				return;
+   			 }
+   			 //write file into textArea
+   			BufferedReader br = new BufferedReader(fr);
+   			String line;
+   			try {
+   				// avoiding newline at the end of file:
+   				// write the first line first
+   				if ((line = br.readLine()) != null) {
+   					ta.append(line);
+   				}
+   				// "\n" before writing a new line
+   				while((line = br.readLine()) != null) {
+   					ta.append("\n" + line);
+   				}
+   				br.close();
+   			} catch(IOException f) {
+   				f.printStackTrace();
+   			}
+   			 
+   		 }
+   	 });
    	 
    	 fileNameBar.setLayout(new BorderLayout());
    	 fileNameBar.add(load, BorderLayout.EAST);
